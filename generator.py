63721d88adbefd50ddd86437d6afd68ec5d8fd86
Emily Salvador <emilysa@mit.edu>
2014-06-23 01:31:51 -0700

    seperated function

--- generator.py
+++ generator.py
@@ -1,24 +1,25 @@
+'''This is the generator for searching two .vcf docs for matching info'''
 import vcf
-
-
-def main():
-    # Set up two file readers:
-    # One for the ClinVar file, one for the genome data.
-    # For now you can just use sys.argv[1], implement
-    # flags later.
+class generator():
+    
     clinvar = "/home/emilysa/ChurchLab/clinvar-latest.vcf"
-    genome = "/home/emilysa/ChurchLab/hu4040B8.vcf"
+    genome = "/home/emilysa/ChurchLab/hu4040B8.vcf"    
     clin_file = vcf.Reader(open(clinvar, "r"))
     genome_file = vcf.Reader(open(genome, "r"))
     # Create a loop where, each step, the file with
     # the earliest line "steps forward".
     clin_record = clin_file.next()
     genome_record = genome_file.next()
-    for genome_record in genome_file:
-
-        #Check to see if chromosome match
-        if clin_record.CHROM == genome_record.CHROM:
-            #If they do, check to see if positions match
+    
+    def display_match():
+        print "Match Record:",
+        print genome_record
+        print "Clin Record:",
+        print clin_record
+        print "\n"
+    
+    def check_pos():
+        for genome_record in genome_file:
             if clin_record.POS > genome_record.POS:
                 #if the ClinVar position is greater, advance the genome's file
                 genome_record = genome_file.next()
@@ -28,12 +29,8 @@ def main():
             else:
                 #Perfect matches
                 if clin_record == genome_record:
-                    print "Perfect Match!"
-                    print "Match Record:",
-                    print genome_record
-                    print "Clin Record:",
-                    print clin_record
-                    print "\n"
+                    #print "Perfect Match!"
+                    #display_match
                     genome_record = genome_file.next()
                     clin_record = clin_file.next()
                 else:
@@ -45,40 +42,40 @@ def main():
                     #If there are multiple ACC#
                     elif genome_record.REF == clin_record.REF:
                         print "Multiple Paths"
-                        print "Match Record:",
-                        print genome_record
-                        print "Clin Record:",
-                        print clin_record
-                        print "\n"
+                        display_match()
                         genome_record = genome_file.next()
                         clin_record = clin_file.next()
                     #If REF has different length
                     else:
                         #If REF has different length, but are longer segments,
                         #include in results
-                        if (len(str(genome_record.ALT)) - len(genome_record.REF) == 2) and (len(str(genome_record.REF)) - len(str(clin_record.REF))) == (len(str(genome_record.ALT)) - len(str(clin_record.ALT))):
+                        genome_alt = str(genome_record.ALT)
+                        genome_ref = str(genome_record.REF)
+                        clin_alt = str(clin_record.ALT)
+                        clin_ref = str(clin_record.REF)
+                        if (len(genome_alt) - len(genome_ref) == 2) and \
+                        len(genome_ref) - len(clin_ref) == \
+                        len(genome_alt) - len(clin_alt):
                             print "REF length inconsistent"
-                            print "Match Record:",
-                            print genome_record
-                            print "Clin Record:",
-                            print clin_record
-                            print "\n"
+                            display_match()
                             genome_record = genome_file.next()
                             clin_record = clin_file.next()
-                        #Don't include if the REF has different length and
+                            #Don't include if the REF has different length and
                             #represents insertion, deletion, etc
                         else:
                             genome_record = genome_file.next()
                             clin_record = clin_file.next()
-        elif clin_record.CHROM > genome_record.CHROM:
-            #If the ClinVar chromosome is greater, advance the genome's file
-            genome_record = genome_file.next()
-        else:
-            #If the genome's chromosome is greater, advance the ClinVar file
-            clin_record = clin_file.next()
-            
-
-if __name__ == "__main__":
-    main()
-
-    
+    def main():
+        for genome_record in genome_file:
+            #Check to see if chromosome match
+            if clin_record.CHROM == genome_record.CHROM:
+                check_pos()
+            elif clin_record.CHROM > genome_record.CHROM:
+                #If the ClinVar chromosome is greater, advance the genome's file
+                genome_record = genome_file.next()
+            else:
+                #If the genome's chromosome is greater, advance the ClinVar file
+                clin_record = clin_file.next()
+                
+    if __name__ == "__main__":
+        main()
