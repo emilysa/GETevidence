4973e4cdda3e09dcbcf0fd9d4eed51595f6a23a3
Emily Salvador <emilysa@mit.edu>
2014-06-27 11:59:53 -0700

    reorganizing, separating filter function

--- generator.py
+++ generator.py
@@ -1,12 +1,11 @@
 '''Generator that takes two .vcf files and displays matches'''
 
 CHROM_INDEX = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
-               "6": 6, "7": 7, "8": 8, "9": 9, "10:" 10,
+               "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
                "11": 11, "12": 12, "13": 13, "14": 14, "15": 15,
                "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
                "21": 21, "22": 22, "X": 23, "Y": 24, "M": 25
            }
-
 CLNSIG_INDEX = {0 : "unknown",
                 1 : "untested",
                 2 : "non-pathogenic",
@@ -17,6 +16,7 @@ CLNSIG_INDEX = {0 : "unknown",
                 7 : "affecting histocompatibility",
                 255 : "other"
                 }
+
 
 def main():
     '''Takes two .vcf files and returns matches'''
@@ -37,6 +37,7 @@ def main():
         global clin_ref
         global clin_line
         global clin_info
+        clin_record = clin_file.next()
         clin_line = clin_record.split("\t")
         clin_chrom = CHROM_INDEX[clin_line[0]]
         clin_pos = int(clin_line[1])
@@ -53,6 +54,7 @@ def main():
         global genome_alt
         global genome_ref
         global genome_line
+        genome_record = genome_file.next()
         genome_line = genome_record.split("\t")
         genome_chrom = CHROM_INDEX[genome_line[0]]
         genome_pos = int(genome_line[1])
@@ -69,7 +71,7 @@ def main():
                 values = split.split(",")
                 for i in values:
                     int_i = int(i)
-                    if int_i >= 5 or int_i == 0:
+                    if int_i >= 4 or int_i == 0:
                         print CLNSIG_INDEX[int_i]
                         return True
     def get_clnalle():
@@ -93,8 +95,38 @@ def main():
             print genome_line[0], genome_pos, genome_ref, genome_alt
             print "Clinvar:",
             print clin_line[0], clin_pos, clin_ref, clin_alt
-            get_clnsig()
             get_clnalle()
+
+    def compare_match():
+        #Perfect matches
+        if clin_alt == genome_alt and clin_ref == genome_ref:
+            print_line()
+        else:
+            #Edge Cases and how we're handling them
+            #If ALT = <CGA_CNVWIN>, filter this out
+            if str(genome_alt) == "<CGA_CNVWIN>":
+                None
+            #If there are multiple ACC#
+            elif genome_ref == clin_ref:
+                clin_alt_split = clin_alt.split(",")
+                genome_alt_split = genome_alt.split(",")
+                for i in clin_alt_split:
+                    for j in genome_alt_split:
+                        if i == j:
+                            print_line()
+                #If REF has different length
+            else:
+            #If REF has different length, but are longer segments,
+            #include in results
+                clin_alt_split = clin_alt.split(",")
+                genome_alt_split = genome_alt.split(",")
+                for i in clin_alt_split:
+                    for j in genome_alt_split:
+                        if len(genome_ref) - \
+                            len(clin_ref) == \
+                            len(j) - \
+                            len(i):
+                            print_line()
     '''Takes two .vcf files and returns matches'''
     #Check to see if chromosome match
     #While the clinvar file is behind the genome file's position
@@ -108,53 +140,19 @@ def main():
             #If they do, check to see if positions match
             if clin_pos > genome_pos:
                 #if the ClinVar position is greater, advance the genome's file
-                genome_record = genome_file.next()
                 genome_forward(genome_record)
             elif clin_pos < genome_pos:
                 #if the genome's position is greater, advance the ClinVar file
-                clin_record = clin_file.next()
                 clin_forward(clin_record)
             else:
-                #Perfect matches 
-                if clin_alt == genome_alt and clin_ref == genome_ref:
-                    print_line()
-                else:
-                    #Edge Cases and how we're handling them
-                    #If ALT = <CGA_CNVWIN>, filter this out
-                    if str(genome_alt) == "<CGA_CNVWIN>":
-                        None
-                    #If there are multiple ACC#
-                    elif genome_ref == clin_ref:
-                        clin_alt_split = clin_alt.split(",")
-                        genome_alt_split = genome_alt.split(",")
-                        for i in clin_alt_split:
-                            for j in genome_alt_split:
-                                if i == j:
-                                    print_line()
-                        #If REF has different length
-                    else:
-                    #If REF has different length, but are longer segments,
-                    #include in results
-                        clin_alt_split = clin_alt.split(",")
-                        genome_alt_split = genome_alt.split(",")
-                        for i in clin_alt_split:
-                            for j in genome_alt_split:
-                                if len(genome_ref) - \
-                                    len(clin_ref) == \
-                                    len(j) - \
-                                    len(i):
-                                    print_line()
-                genome_record = genome_file.next()
+                compare_match()
                 genome_forward(genome_record)
-                clin_record = clin_file.next()
                 clin_forward(clin_record)
         elif clin_chrom > genome_chrom:
         #If the ClinVar chromosome is greater, advance the genome's file
-            genome_record = genome_file.next()
             genome_forward(genome_record)
         else:
         #If the genome's chromosome is greater, advance the ClinVar file
-            clin_record = clin_file.next()
             clin_forward(clin_record)
 
 if __name__ == "__main__":
