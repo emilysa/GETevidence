74b62e7b641fda62cf26168930da79132067c338
Emily Salvador <emilysa@mit.edu>
2014-07-07 12:40:06 -0700

    fixed getclinvar to unzip file

--- getclinvar.py
+++ getclinvar.py
@@ -1,4 +1,12 @@
 import urllib
+import gzip
 
 urllib.urlretrieve("ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar-latest.vcf.gz",
-                       "clinvar-latest.vcf")
+                       "gzip.gz")
+
+f = gzip.open('gzip.gz', 'r')
+vcffile = open('clinvar-latest.vcf', 'w')
+file_content = f.read()
+vcffile.write(file_content)
+f.close()
+vcffile.close()
